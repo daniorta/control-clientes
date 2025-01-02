df94f9a29f7a699ae270e5becbcfae28ef1f7218
Dani <148881254+daniorta@users.noreply.github.com>
2025-01-02 12:13:26 +0100

    eliminar cliente

--- src/app/servicios/cliente.service.ts
+++ src/app/servicios/cliente.service.ts
@@ -1,8 +1,8 @@
 import { Injectable } from '@angular/core';
 import { Cliente } from '../modelo/cliente.modelo';
 import { Observable } from 'rxjs';
-import { collection, collectionData, Firestore, orderBy, query } from '@angular/fire/firestore';
-import { addDoc } from 'firebase/firestore';
+import { collection, collectionData, docData, Firestore, orderBy, query } from '@angular/fire/firestore';
+import { addDoc, deleteDoc, doc, updateDoc,  } from 'firebase/firestore';
 
 @Injectable({
   providedIn: 'root'
@@ -26,6 +26,21 @@ export class ClienteService {
   agregarCliente(cliente: Cliente){
     // agregar un nuevo documento a nuetra coleccion addDoc
     return addDoc(this.clientesRef, cliente )
+  }
+
+  getCliente(id: string): Observable<Cliente | null>{
+    const clienteDocRef = doc(this.firestore, `cliente/${id}`); // con el 'doc', recuperamos la referencia del documento
+    return docData(clienteDocRef, {idField: 'id'}) as Observable<Cliente>;
+  }
+
+  modificarCliente(cliente:Cliente){
+      const clienteDoc = doc(this.firestore, `clientes/${cliente.id}`);
+      return updateDoc(clienteDoc, {...cliente});
+  }
+
+  eliminarCliente(cliente: Cliente){
+    const clienteDoc = doc(this.firestore, `clientes/${cliente.id}`);
+    return deleteDoc(clienteDoc);
   }
 
 }
